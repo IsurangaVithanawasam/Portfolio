59b87e9e995eaa1101d744ee805f214385de31b9
IsurangaVithanawasam <140340209+IsurangaVithanawasam@users.noreply.github.com>
2026-01-29 11:04:22 +0530

    Update copyright year in Footer component

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = ({isDarkMode}) => {
 
         <div className='text-center sm:flex items-center justify-between border-t
         border-gray-400 mx-[10%] mt-12 py-6'>
-            <p>© 2025 Isuranga Vithanawasam. All rights reserved.</p>
+            <p>© 2026 Isuranga Vithanawasam. All rights reserved.</p>
             <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
                 <li><a target='_blank' href="https://github.com/IsurangaVithanawasam">Github</a></li>
                 <li><a target='_blank' href="https://www.linkedin.com/in/isurangavithanawasam">LinkedIn</a></li>
